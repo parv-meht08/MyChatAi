46d347709c534707d32c348fb958b6c9b5be2407
Parv Mehta <mehtaparv21@gmail.com>
2025-01-30 12:35:35 +0530

    Update 4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import { UserProvider } from "./context/user.context";
+import { getWebContainer } from "./config/webContainer";
 
 const App = () => {
   return (
